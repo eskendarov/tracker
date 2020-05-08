b3442b0340176a8253ee4a630e0c34c0c4da58e7
Eskendarov <envereskendarov@gmail.com>
2020-05-08 18:34:44 +0300

    5. Tracker - хранилище[#272217]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,29 +1,54 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
+    private int position = 0;
     private int size = 0;
 
     public Item add(Item item) {
         item.setId(generateId());
-        items[size++] = item;
+        items[position++] = item;
         return item;
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (Item item : items) {
             if (item.getId().equals(id)) {
                 rsl = item;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        size = 0;
+        Item[] withoutNull = new Item[items.length];
+        for (Item item : items) {
+            if (null != item) {
+                withoutNull[size++] = item;
+            }
+        }
+        return Arrays.copyOf(withoutNull, size);
+    }
+
+    public Item[] findByName(String key) {
+        size = 0;
+        Item[] withKey = new Item[items.length];
+        for (int i = 0; i < position; i++) {
+            if (key.equals(items[i].getName())) {
+                withKey[size++] = items[i];
+            }
+        }
+        return Arrays.copyOf(withKey, size);
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,16 +1,56 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    public void addItems() {
         Tracker tracker = new Tracker();
-        Item item = new Item("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+        Item item1 = new Item("Java");
+        Item item2 = new Item("Android");
+        tracker.add(item1);
+        tracker.add(item2);
+        Item result = tracker.findById(item1.getId());
+        assertThat(result.getName(), is(item1.getName()));
+    }
+
+    @Test
+    public void findById() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Java");
+        Item item2 = new Item("Android");
+        tracker.add(item1);
+        tracker.add(item2);
+        Item result = tracker.findById(item1.getId());
+        assertThat(result.getName(), is("Java"));
+    }
+
+    @Test
+    public void findAll() {
+        Item item1 = new Item("Java");
+        Item item2 = new Item("Android");
+        Item[] expected = {item1, item2};
+        Tracker tracker = new Tracker();
+        tracker.add(item1);
+        tracker.add(item2);
+        Item[] rsl = tracker.findAll();
+        assertThat(expected, is(rsl));
+    }
+
+    @Test
+    public void findByName() {
+        Item item1 = new Item("Item1");
+        Item item2 = new Item("Item2");
+        Item item3 = new Item("Item1");
+        Item[] expected = {item1, item3};
+        Tracker tracker = new Tracker();
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] rsl = tracker.findByName("Item1");
+        assertThat(expected, is(rsl));
     }
 }
