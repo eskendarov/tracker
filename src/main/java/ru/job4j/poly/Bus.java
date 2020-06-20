264b20a2eb4bd94c5ba94e70d4f15795db673b2a
Eskendarov <envereskendarov@gmail.com>
2020-06-20 16:26:28 +0300

    3. Интерфейс Input[181092#272155]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,28 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    private double priceForLit = 46.25;
+
+    public double getPriceForLit() {
+        return priceForLit;
+    }
+
+    public void setPriceForLit(double priceForLit) {
+        this.priceForLit = priceForLit;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Driving");
+    }
+
+    @Override
+    public void passengers(int num) {
+        System.out.println(num);
+    }
+
+    @Override
+    public double refuel(double liters) {
+        return liters * priceForLit;
+    }
+}
