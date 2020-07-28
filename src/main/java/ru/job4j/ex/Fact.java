7eee4566c4b93ee4c77575f1caa3e8cb8f30ba5a
Eskendarov <envereskendarov@gmail.com>
2020-07-28 16:42:27 +0300

    0.3. Тестирование исключений с junit[219366#272173]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -8,7 +8,7 @@ public class Fact {
     public int calc(int n) {
         int rsl = 1;
         if (n < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Start should be less then finish.");
         }
         for (int index = 1; index <= n; index++) {
             rsl *= index;
