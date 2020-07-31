a53d78840c0b1d0d621ed4371e3992458bdc1a51
Eskendarov <envereskendarov@gmail.com>
2020-07-31 08:27:23 +0300

    1.1. Тесты на ValidateInput.[223313#272177]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -140,4 +140,26 @@ public class StartUITest {
         new StartUI(output).init(input, tracker, actions);
         assertThat(output.toString(), is(expected));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1","0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu:%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu:%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
 }
