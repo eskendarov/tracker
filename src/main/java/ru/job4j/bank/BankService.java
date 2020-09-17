0998082864f7a6fa37ec6170b74c2b8cc71086c3
Eskendarov <envereskendarov@gmail.com>
2020-09-18 01:17:44 +0300

    3. Банковские переводы.[10038#272142]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,53 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        users.get(user).add(account);
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (users.containsKey(user)) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account src = findByRequisite(srcPassport, srcRequisite);
+        Account dest = findByRequisite(destPassport, destRequisite);
+        if (null != src.getRequisite() && src.getBalance() >= amount) {
+            dest.setBalance(dest.getBalance() + amount);
+            src.setBalance(src.getBalance() - amount);
+            return true;
+        }
+        return false;
+    }
+}
