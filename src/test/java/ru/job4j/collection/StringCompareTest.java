4d11ab63f72c8f6ef9d018f01b69b55ba1b91858
Eskendarov <envereskendarov@gmail.com>
2020-09-20 11:51:34 +0300

    3. Компаратор для строк[35008#272126]

---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,92 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class StringCompareTest {
+
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void test1() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "",
+                " "
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void test2() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                " Adff",
+                " ."
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void test3() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "",
+                ""
+        );
+        assertEquals(rst, 0);
+    }
+}
