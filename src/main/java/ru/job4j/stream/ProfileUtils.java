7d0692ac4deab8f5dbbdcf8a3c9368f92f74f74b
Eskendarov <envereskendarov@gmail.com>
2020-10-07 15:36:30 +0300

    3. Уникальность элементов и сортировка.[110225#272001] Метод uniqueAddress() вынесен в класс ProfileUtils

--- src/main/java/ru/job4j/stream/ProfileUtils.java
+++ src/main/java/ru/job4j/stream/ProfileUtils.java
@@ -8,4 +8,8 @@ public class ProfileUtils {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
+
+    public List<Address> uniqueAddress(List<Address> addresses) {
+        return addresses.stream().sorted(Address::compareTo).distinct().collect(Collectors.toList());
+    }
 }
--- src/test/java/ru/job4j/stream/ProfileUtilsTest.java
+++ src/test/java/ru/job4j/stream/ProfileUtilsTest.java
@@ -22,4 +22,20 @@ public class ProfileUtilsTest {
         );
         assertThat(new ProfileUtils().collect(profileList), is(expected));
     }
+
+    @Test
+    public void getSortedUniqueAddresses() {
+        List<Address> addresses = Arrays.asList(
+                new Address("SPb", "Ushakova", 55, 234),
+                new Address("SPb", "Ushakova", 55, 234),
+                new Address("Los Angeles", "Moskovskaya", 87, 1234),
+                new Address("Moscow", "Sadovaya", 2, 112)
+        );
+        List<Address> expected = Arrays.asList(
+                new Address("Los Angeles", "Moskovskaya", 87, 1234),
+                new Address("Moscow", "Sadovaya", 2, 112),
+                new Address("SPb", "Ushakova", 55, 234)
+        );
+        assertThat(new ProfileUtils().uniqueAddress(addresses), is(expected));
+    }
 }
