70df5ece75aa3e29d13e4af85d9cfc92f9f8e4d3
Eskendarov <envereskendarov@gmail.com>
2021-03-16 22:22:24 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy. [#389130]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -23,12 +23,57 @@ public class SqlTrackerTest {
     }
 
     @Test
-    public void createItem() throws Exception {
+    public void add() throws Exception {
         try (SqlTracker tracker = new SqlTracker(
                 ConnectionRollback.create(this.init()))
         ) {
-            tracker.add(new Item("name", "desc"));
+            tracker.add(new Item("desc"));
             assertThat(tracker.findByName("desc").size(), is(1));
+        }
+    }
+
+    @Test
+    public void replace() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(
+                ConnectionRollback.create(this.init()))
+        ) {
+            final Item item = tracker.add(new Item("desc"));
+            tracker.replace(item.getId(), new Item("newItem"));
+            assertThat(tracker.findById(item.getId()).getName(),
+                    is("newItem"));
+        }
+    }
+
+    @Test
+    public void delete() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(
+                ConnectionRollback.create(this.init()))
+        ) {
+            final Item item = tracker.add(new Item("desc"));
+            tracker.delete(item.getId());
+            assertThat(tracker.findByName("desc").size(), is(0));
+        }
+    }
+
+    @Test
+    public void findById() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(
+                ConnectionRollback.create(this.init()))
+        ) {
+            final Item item = tracker.add(new Item("byId"));
+            assertThat(tracker.findById(item.getId()).getName(),
+                    is(item.getName()));
+        }
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(
+                ConnectionRollback.create(this.init()))
+        ) {
+            final Item item = tracker.add(new Item("byName"));
+            assertThat(tracker.findByName("byName").get(0).getId(),
+                    is(item.getId()));
         }
     }
 }
